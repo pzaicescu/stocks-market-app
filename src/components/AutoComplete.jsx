df89b1257051fb137b17188673591d074bc67e84
Zaicescu Paul <paul.zaicescu@tickbird.org>
2022-11-08 17:20:54 +0200

    adding the autocomplete component

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import finHub from "../apis/finHub";
 
 const AutoComplete = () => {
+    const [search, setSearch] = useState("")
+    const [results, setResults] = useState([])
+
+    const renderDropdown = () => {
+        const dropDownClass = search ? "show" : null
+        return (
+            <ul style={{
+                height: "500px",
+                overflowY: "scroll",
+                overflowX: "hidden",
+                cursor: "pointer"
+                }}
+                className={`dropdown-menu ${dropDownClass}`}>
+                {results.map((result) => {
+                    return (<li key={result.symbol} className="dropdown-item">{result.description} ({result.symbol})</li>)
+                })}
+            </ul>
+        )
+    }
+
+    useEffect(() => {
+        let isMounted = true
+        const fetchData = async () => {
+            try {
+                const response = await finHub.get("/search", {
+                    params: {
+                        q: search
+                    }
+                })
+                if (isMounted) {
+                    setResults(response.data.result)
+                }
+            } catch (err) {
+
+            }
+        }
+        if (search.length > 0) {
+            fetchData()
+        } else {
+            setResults([])
+        }
+        return () => (isMounted = false)
+    }, [search])
+
     return (
-        <div>
-            AutoComplete
+        <div className="w-50 p-5 rounded mx-auto">
+            <div className="form-floating dropdown">
+                <input type="text" style={{backgroundColor: "rgba(145, 158, 171, 0.04)"}} id="search"
+                       className="form-control" placeholder="Search" autoComplete="off" value={search}
+                       onChange={(e) => setSearch(e.target.value)}/>
+                <label htmlFor="search">Search</label>
+                {renderDropdown()}
+            </div>
         </div>
     );
 };
